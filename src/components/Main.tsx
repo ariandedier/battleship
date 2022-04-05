e9e450fa688f3aeb484ef67328a7b31743066caa
ariandedier <ariandedierfea@gmail.com>
2022-04-05 21:46:10 +0100

    Main component

---
+++ src/components/Main.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+
+const Main = () => {
+  return (
+    <MainWrapper>
+      
+    </MainWrapper>
+  )
+}
+
+const MainWrapper = styled.main`
+  padding: ${({ theme }) => theme.padding.lg};
+`
+
+export default Main
